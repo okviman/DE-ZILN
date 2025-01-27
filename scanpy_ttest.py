Fixed t-test computation in utils.py.

scanpy_ttest.py
@@ -16,6 +16,7 @@ def scanpy_sig_test(X, Y, method='t-test'):
     adata.var_names = [f"Gene{i}" for i in range(n_genes)]  # Gene names
     adata.obs["group"] = np.concatenate([X_group, Y_group])  # Assign group labels
 
+    adata.layers["counts"] = adata.X.copy()
     sc.pp.normalize_total(adata, target_sum=1e4)
     sc.pp.log1p(adata)
 
@@ -24,13 +25,15 @@ def scanpy_sig_test(X, Y, method='t-test'):
 
     # Extract DE Results
     de_results = pd.DataFrame({
-        "gene": adata.var_names,
+        "gene": adata.uns['rank_genes_groups']['names']['Y'],
         "log2_fc": adata.uns["rank_genes_groups"]["logfoldchanges"]["Y"],
         "p_value": adata.uns["rank_genes_groups"]["pvals"]["Y"],
         "p_adj": adata.uns["rank_genes_groups"]["pvals_adj"]["Y"]
     })
+    de_results["gene"] = [int(gene.replace("Gene", "")) for gene in de_results["gene"]]
+    gene_idx_sorted = np.argsort(de_results["gene"])
 
-    return de_results["log2_fc"], de_results["p_value"]
+    return de_results["log2_fc"][gene_idx_sorted], de_results["p_value"][gene_idx_sorted]
 
 
 def get_test_results(adj_p_vals, true_lfcs):
@@ -45,6 +48,8 @@ def get_test_results(adj_p_vals, true_lfcs):
     fnr = fn / (fn + tp) if (fn + tp) > 0 else 0  # False Negative Rate
     precision = tp / (tp + fp) if (tp + fp) > 0 else 0
     recall = tp / (tp + fn) if (tp + fn) > 0 else 0
+    f1 = 2 * (precision * recall) / (precision + recall) if (precision + recall) > 0 else 0
+    accuracy = np.sum((gt_sig_idx == pred_sig_idx)) / gt_sig_idx.size
 
     # Print results
     print(f"TPR: {tpr:.2f}")
@@ -53,6 +58,8 @@ def get_test_results(adj_p_vals, true_lfcs):
     print(f"FNR: {fnr:.2f}")
     print(f"Precision: {precision:.2f}")
     print(f"Recall: {recall:.2f}")
+    print(f"F1: {f1:.2f}")
+    print(f"Accuracy: {accuracy:.2f}")
     print()
 
 
@@ -60,15 +67,17 @@ np.random.seed(0)
 nx = 1000
 ny = 1000
 n_genes = 1500
-mu1 = 100 + np.random.normal(0, 10, (1, n_genes)) * np.random.binomial(1, 0.1, (1, n_genes))
-mu2 = 100 + np.random.normal(0, 20, (1, n_genes)) * np.random.binomial(1, 0.1, (1, n_genes))
-d1 = 10
-d2 = 1
+mu1 = 10 + np.abs(np.random.normal(0, 1, (1, n_genes))) * np.random.binomial(1, 0.1, (1, n_genes))
+mu2 = 10  # + np.random.normal(0, 1, (1, n_genes)) * np.random.binomial(1, 0.1, (1, n_genes))
+d1 = 0.2
+d2 = 0.1
 
 r1 = 1 / d1
 r2 = 1 / d2
 p1 = 1 / (1 + d1 * mu1)
 p2 = 1 / (1 + d2 * mu2)
+
+print("Variance X: ", np.mean((1 - p1) * r1 / (p1 ** 2)), "Other parameterization: ", np.mean(mu1 + mu1 ** 2 * d1))
 
 # Generate synthetic gene expression data
 X = np.random.negative_binomial(n=r1, p=p1, size=(nx, n_genes))
@@ -76,28 +85,41 @@ Y = np.random.negative_binomial(n=r2, p=p2, size=(ny, n_genes))
 
 true_lfcs = np.log2(mu2 / mu1)
 
-# scanpy in raw space
 sc_lfcs, sc_p_vals = scanpy_sig_test(X, Y)
 sc_adj_pvals = smm.multipletests(sc_p_vals, alpha=0.05, method='fdr_bh')[1]
-plt.scatter(sc_lfcs, -np.log10(sc_adj_pvals))
-plt.scatter(true_lfcs, -np.log10(sc_adj_pvals))
-plt.axhline(-np.log10(0.05), -1000, 1000, color='red', label='adj $p$ < 0.05')
-plt.ylabel("$-\log_{10}(p)$")
-plt.xlabel('Estimated LFC')
-plt.legend()
+_, ax = plt.subplots(1, 2, figsize=(10, 10))
+ax[0].scatter(sc_lfcs, -np.log10(sc_adj_pvals))
+ax[0].scatter(true_lfcs, -np.log10(sc_adj_pvals))
+ax[0].axhline(-np.log10(0.05), -1000, 1000, color='red', label='adj $p$ < 0.05')
+ax[0].set_ylabel("$-\log_{10}(p)$")
+ax[0].set_xlabel('Estimated LFC')
+ax[0].legend()
+ax[1].scatter(true_lfcs, sc_lfcs, label='Estimated LFCs')
+ax[1].scatter(true_lfcs, true_lfcs, label='True LFCs')
+ax[1].scatter(true_lfcs[0, sc_adj_pvals < 0.05], sc_lfcs[sc_adj_pvals < 0.05], color='red', label='DE Classified')
+ax[1].legend()
+plt.suptitle('Scanpy')
 plt.show()
 print("Scanpy Test Results: ")
 get_test_results(sc_adj_pvals, true_lfcs)
 
 
-DELN_lfcs, DELN_p_vals = get_DELN_lfcs(X, Y, test='z')
+X = 1e4 * X / X.sum(1, keepdims=True)
+Y = 1e4 * Y / Y.sum(1, keepdims=True)
+DELN_lfcs, DELN_p_vals = get_DELN_lfcs(Y, X, test='t')
 DELN_adj_pvals = smm.multipletests(DELN_p_vals, alpha=0.05, method='fdr_bh')[1]
-plt.axhline(-np.log10(0.05), -1000, 1000, color='red', label='adj $p$ < 0.05')
-plt.scatter(DELN_lfcs, -np.log10(DELN_adj_pvals), label='DELN LFC vs DELN adj $p$')
-plt.scatter(true_lfcs, -np.log10(DELN_adj_pvals), label='True LFC vs DELN adj $p$')
-plt.ylabel("$-\log_{10}(p)$")
-plt.legend()
-plt.xlabel('Estimated LFC')
+_, ax = plt.subplots(1, 2, figsize=(10, 10))
+ax[0].scatter(DELN_lfcs, -np.log10(DELN_adj_pvals), label='DELN LFC vs DELN adj $p$')
+ax[0].scatter(true_lfcs, -np.log10(DELN_adj_pvals), label='True LFC vs DELN adj $p$')
+ax[0].axhline(-np.log10(0.05), -1000, 1000, color='red', label='adj $p$ < 0.05')
+ax[0].set_ylabel("$-\log_{10}(p)$")
+ax[0].legend()
+ax[0].set_xlabel('Estimated LFC')
+ax[1].scatter(true_lfcs, DELN_lfcs, label='Estimated LFCs')
+ax[1].scatter(true_lfcs, true_lfcs, label='True LFCs')
+ax[1].scatter(true_lfcs[0, DELN_adj_pvals < 0.05], sc_lfcs[DELN_adj_pvals < 0.05], color='red', label='DE Classified')
+ax[1].legend()
+plt.suptitle('DELN')
 plt.show()
 
 print("DELN Test Results: ")

utils.py
@@ -1,6 +1,6 @@
 import numpy as np
 import scipy.stats as stats
-
+from scipy.stats.distributions import t
 
 def digamma(x):
     return np.log(x) - 1 / (2 * x)
@@ -174,7 +174,8 @@ def get_DELN_lfcs(Y_, X_, normalize=True, test='t'):
     se_X = np.sqrt(se_X_1 + se_X_2) / np.log(2)
 
     if test == 't':
-        statistic, p_vals = get_t_statistic(log2_theta_hat_Y + log2_m_Y, log2_theta_hat_X + log2_m_X, se_Y, se_X)
+        statistic, p_vals = get_t_statistic(log2_theta_hat_Y + log2_m_Y, log2_theta_hat_X + log2_m_X,
+                                            se_Y, se_X, n, n_prime)
     else:
         # z-test
         statistic, p_vals = compute_p_vals(log2_theta_hat_Y + log2_m_Y, log2_theta_hat_X + log2_m_X, se_Y, se_X)
@@ -238,5 +239,14 @@ def compute_p_vals(mean1, mean2, se1, se2):
 
     return z_stat, p_value
 
-def get_t_statistic(mean1, mean2, se1, se2):
-    return stats.ttest_ind_from_stats(mean1, se1, 1, mean2, se2, 1, equal_var=False)
+def get_t_statistic(mean1, mean2, se1, se2, n1, n2):
+    # implements two-sided t-test
+    nu1, nu2 = n1 - 1, n2 - 1
+    df = (se1 ** 2 + se2 ** 2) ** 2 / (se1 ** 4 / nu1 + se2 ** 4 / nu2)
+    d = mean1 - mean2
+    denom = ((se1 ** 2 + se2 ** 2) ** 0.5)
+    t_statistic = d / denom
+    t_dist = t(df)
+    p_value = 2 * t_dist.sf(np.abs(t_statistic))
+    return t_statistic, p_value
+